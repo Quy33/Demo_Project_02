ff18036b9fe56f3c2a70c093343476144a8e42ca
Quy <quyhd@mcrew-tech.com>
2021-07-13 21:03:19 +0700

    Initial Commit

---
+++ Demo_Project_02Tests/Demo_Project_02Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Demo_Project_02Tests.swift
+//  Demo_Project_02Tests
+//
+//  Created by Mcrew Tech on 13/07/2021.
+//
+
+import XCTest
+@testable import Demo_Project_02
+
+class Demo_Project_02Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
